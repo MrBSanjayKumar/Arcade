dbb41d357b1e0bf454754b95ed7b3699d28d6fd3
SANJAY KUMAR <124190579+MrBSanjayKumar@users.noreply.github.com>
2025-09-24 08:42:52 +0530

    Add files via upload

---
+++ src/types/Game.js
@@ -0,0 +1,18 @@
+/**
+ * @typedef {Object} Game
+ * @property {number} id
+ * @property {string} title
+ * @property {string} description
+ * @property {number} price
+ * @property {number} originalPrice
+ * @property {string} image
+ * @property {string} category
+ * @property {number} rating
+ * @property {number} reviews
+ * @property {number} discount
+ * @property {string} releaseDate
+ * @property {string} developer
+ * @property {string[]} tags
+ */
+
+// This file is only for documentation/IntelliSense. No export needed.
